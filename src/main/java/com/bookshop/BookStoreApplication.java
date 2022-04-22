6086241ae18c58236b3c637f0fe5a26d4046e274
tsholofelomoema <tsholofelomoema@192.168.0.200>
2022-04-22 04:42:21 +0200

    Added security

--- src/main/java/com/bookshop/BookStoreApplication.java
+++ src/main/java/com/bookshop/BookStoreApplication.java
@@ -2,8 +2,10 @@ package com.bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
